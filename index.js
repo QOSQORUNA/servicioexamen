244f874addc75abcc5960974e5f2a2aed12a6e5c
qosqoruna1996 <jose.lovaton@unas.edu.pe>
2020-09-01 16:06:20 -0500

    para heroku

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 const apiV1 = require('./api/v1/routes');
 
 app.use('/api/v1/score', apiV1);
 
 
-app.listen(3000, () => {
-    console.log('Server running in 3000');
-})
+// app.listen(3000, () => {
+//     console.log('Server running in 3000');
+// })
+app.listen(port, function() {
+    console.log('Servidorciado en el puerto 8080.');
+   });
