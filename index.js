8d4b655002456d1fcddafec7222808d74b385ace
qosqoruna1996 <jose.lovaton@unas.edu.pe>
2020-09-01 19:38:48 -0500

    update

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var port = process.env.PORT || 8080;
 
 const apiV1 = require('./api/v1/routes');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use('/api/v1/score', apiV1);
 
 
