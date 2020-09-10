686349e8b736ac8d642e93d90068e7eab314dba2
qosqoruna1996 <jose.lovaton@unas.edu.pe>
2020-09-10 12:05:05 -0500

    first commit

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,127 +1,158 @@
 var express = require('express');
 var router = express.Router();
 
-const coursesArray = [
+const proveedorArray = [
     {
         id: 1,
-        name: 'programación',
-    },
-    {
-        id: 2,
-        name: 'matemática'
-    },
-    {
-        id: 3,
-        name: 'inglés'
-    },
-    {
-        id: 4,
-        name: 'Integracion'
-    },
-    {
-        id: 5,
-        name: 'Tesis'
-    }
-]
-
-const studentsArray = [
-    {
-        id: 1,
+        dni: '71002152',
         name: 'Fernando',
-        courseId: 1
+        lastName: 'Lovaton Lopez',
+        sancion: true,
+        famMuni: true,
+        famCongreso: true,
+        estadoProv: false,
+        rubro: 'abarrotes'
     },
     {
         id: 2,
-        name: 'Osso',
-        courseId: 2
+        dni: '71002153',
+        name: 'Jose',
+        lastName: 'Lovaton Lopez',
+        sancion: true,
+        famMuni: false,
+        famCongreso: false,
+        estadoProv: false,
+        rubro: 'software'
     },
     {
         id: 3,
-        name: 'Correlon',
-        courseId: 3
+        dni: '71002154',
+        name: 'lourdes',
+        lastName: 'Lopez Castillo',
+        sancion: false,
+        famMuni: false,
+        famCongreso: false,
+        estadoProv: true,
+        rubro: 'construccion'
     },
     {
         id: 4,
-        name: 'Nayhua',
-        courseId: 2
+        dni: '71002155',
+        name: 'David',
+        lastName: 'Lovaton Lopez',
+        sancion: false,
+        famMuni: false,
+        famCongreso: false,
+        estadoProv: true,
+        rubro: 'libreria'
     },
     {
         id: 5,
-        name: 'Zurdo',
-        courseId: 1
+        dni: '71002156',
+        name: 'Jhon',
+        lastName: 'Rupay Eugenio',
+        sancion: true,
+        famMuni: true,
+        famCongreso: true,
+        estadoProv: false,
+        rubro: 'abarrotes'
     },
     {
         id: 6,
-        name: 'lopez',
-        courseId: 2
+        dni: '71002157',
+        name: 'Alejandro',
+        lastName: 'Rupay Fasabi',
+        sancion: false,
+        famMuni: false,
+        famCongreso: false,
+        estadoProv: true,
+        rubro: 'abarrotes'
     },
     {
         id: 7,
-        name: 'Jose',
-        courseId: 5
+        dni: '71002158',
+        name: 'Brian',
+        lastName: 'Pando',
+        sancion: true,
+        famMuni: true,
+        famCongreso: true,
+        estadoProv: true,
+        rubro: 'abarrotes'
     },
-    {
-        id: 8,
-        name: 'lopez Lovaton',
-        courseId: 4
-    }
 ]
 
-router.get('/course', function(req, res) {
+
+
+router.get('/prov', function(req, res) {
     res.status(200).json({
-        content: coursesArray
+        content: proveedorArray
     })
 });
 
-router.get('/course/:courseId', function(req, res) {
+router.get('/prov/:provId', function(req, res) {
 
-    const course = coursesArray.find(item => item.id == req.params.courseId)
+    const provId = proveedorArray.find(item => item.id == req.params.provId)
     
-    if (!course) return res.status(404).json({
+    if (!provId) return res.status(404).json({
         errors: [
             'Recurso no encontrado'
         ]
     })
 
     res.status(200).json({
-        content: course
+        content: provId
     })
 
 });
+router.get('/prov/dni/:provDni', function(req, res) {
 
-router.get('/course/:courseId/student', function(req, res) {
-
-    const course = coursesArray.find(item => item.id == req.params.courseId)
-
-    if (!course) return res.status(404).json({
-        errors: [
-            'Recurso no encontrado'
-        ]
-    })
-
-    const students = studentsArray.filter(item => item.courseId == req.params.courseId)
-
-    res.status(200).json({
-        content: {course, students}
-    })
+    const provDni = proveedorArray.find(item => item.dni == req.params.provDni)
     
-});
-
-router.get('/course/:courseId/student/:studentId', function(req, res) {
-
-    const student = studentsArray.find(item => item.courseId == req.params.courseId && item.id == req.params.studentId)
-
-    if (!student) return res.status(404).json({
+    if (!provDni) return res.status(404).json({
         errors: [
             'Recurso no encontrado'
         ]
     })
 
-    const course = coursesArray.find(item => item.id == req.params.courseId)
-
     res.status(200).json({
-        content: {course, student}
+        content: provDni
     })
-})
+
+});
+
+// router.get('/course/:courseId/student', function(req, res) {
+
+//     const course = coursesArray.find(item => item.id == req.params.courseId)
+
+//     if (!course) return res.status(404).json({
+//         errors: [
+//             'Recurso no encontrado'
+//         ]
+//     })
+
+//     const students = studentsArray.filter(item => item.courseId == req.params.courseId)
+
+//     res.status(200).json({
+//         content: {course, students}
+//     })
+    
+// });
+
+// router.get('/course/:courseId/student/:studentId', function(req, res) {
+
+//     const student = studentsArray.find(item => item.courseId == req.params.courseId && item.id == req.params.studentId)
+
+//     if (!student) return res.status(404).json({
+//         errors: [
+//             'Recurso no encontrado'
+//         ]
+//     })
+
+//     const course = coursesArray.find(item => item.id == req.params.courseId)
+
+//     res.status(200).json({
+//         content: {course, student}
+//     })
+// })
 
 module.exports = router;
